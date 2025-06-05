73130f9576ca76dae225bbd93400fcba91b9ffe8
Luiz Domingues <luiz.f.l.domingues@gmail.com>
2025-06-05 16:46:51 -0300

    improve header

--- conpecome/src/app/(pages)/carrossel/page.tsx
+++ conpecome/src/app/(pages)/carrossel/page.tsx
@@ -2,21 +2,20 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-[#FFE8DE]">
-      {/* Header */}
-      <header className="flex items-center justify-between px-4 py-2 bg-[#FFE8DE]">
+    <div className="min-h-screen bg-[#FFF5EF]">
+      <header className="flex items-center justify-between px-4 py-6 bg-[#FFE8DE] rounded-b-3xl">
         <div className="flex items-center gap-2">
           <Image
-            src="/carrossel/assets/shopping_cart.png"
+            src="/assets/logo-conpec.svg"
             alt="Conpec Logo"
             width={40}
             height={40}
           />
           <div className="flex flex-col">
-            <h1 className="font-pixelify-sans text-2xl text-[#FF3D00]">
+            <h1 className="font-pixelify-sans text-4xl text-[#FF3D00]">
               CONPECOME
             </h1>
-            <p className="text-sm">Já pode aomossar?</p>
+            <p className="text-sm text-[#FF3D00]">Já pode aomossar?</p>
           </div>
         </div>
 
@@ -38,18 +37,17 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="flex items-center gap-2 bg-[#FF9633] px-4 py-2 rounded-full text-white">
+        <div className="shadow-md flex items-center gap-2 bg-[#FFD8B6] border border-[#F54B00] px-4 py-2 rounded-full text-[#F54B00]">
           <Image
             src="/carrossel/assets/shopping_cart.png"
             alt="Shopping Cart"
             width={24}
             height={24}
           />
-          <span>R$ 2,37</span>
+          <span id="cart_total">R$ 23,1</span>
         </div>
       </header>
 
-      {/* Main Content */}
       <main className="flex flex-col gap-8 items-center sm:items-start p-8">
         <div className="text-5xl text-black font-pixelify-sans">
           Carrossel 🦣
