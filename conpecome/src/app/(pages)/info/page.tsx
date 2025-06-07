052c0bed03f0fbc96303505e35e801ed99154bb8
JeordeConpec <jeorde.neto@conpec.com.br>
2025-06-06 22:01:49 -0300

    Update no page.tsx pra correção de erros
    Input da cor correta quando escrevendo, espaçamento apropriado do botão e extrabold nas fontes

--- conpecome/src/app/(pages)/info/page.tsx
+++ conpecome/src/app/(pages)/info/page.tsx
@@ -37,15 +37,15 @@ export default function Login(){
 
                 <div className="w-1/3 grid grid-cols-3 grid-rows-2 gap-6">
                     <div className="grid grid-cols-1 col-span-3 gap-3 items-center">
-                        <label className="text-[#FF7D02] col-span-1 font-poppins text-[16px] font-bold ml-5">Nome</label>
-                        <input type="text" placeholder="Inserir Nome" className="placeholder-[#ff7c0287] font-poppins col-span-3 h-14 rounded-full px-8 py-5 text-[10px] font-bold border-2 border-[#ff5a0179] focus:border-[#ff5a01c9] focus:outline-none" />
+                        <label className="text-[#FF7D02] col-span-1 font-poppins text-[16px] font-extrabold ml-5">Nome</label>
+                        <input type="text" placeholder="Inserir Nome" className="text-[#ff7c0287] placeholder-[#ff7c0287] font-poppins col-span-3 h-14 rounded-full px-8 py-5 text-[10px] font-extrabold border-2 border-[#ff5a0179] focus:border-[#ff5a01c9] focus:outline-none" />
                     </div>
                     <div className="grid grid-cols-1 gap-3 col-span-3 items-center">
-                        <label className="text-[#FF7D02] col-span-1 font-poppins text-[16px] font-bold ml-5">Email</label>
-                        <input type="text" placeholder="Inserir Email" className=" placeholder-[#ff7c0287] font-poppins col-span-3 h-14 rounded-full px-8 py-5 text-[10px] font-bold border-2 border-[#ff5a0179] focus:border-[#ff5a01c9] focus:outline-none" />
+                        <label className="text-[#FF7D02] col-span-1 font-poppins text-[16px] font-extrabold ml-5">Email</label>
+                        <input type="text" placeholder="Inserir Email" className="text-[#ff7c0287] placeholder-[#ff7c0287] font-poppins col-span-3 h-14 rounded-full px-8 py-5 text-[10px] font-extrabold border-2 border-[#ff5a0179] focus:border-[#ff5a01c9] focus:outline-none" />
                     </div>
                         
-                        <button className="font-poppins rounded-full bg-[#ff5a01c3] col-start-2 h-12">Enviar</button>
+                        <button className="font-poppins rounded-full bg-[#ff5a01c3] col-start-2 h-12 mt-10 font-extrabold">Enviar</button>
 
                 </div>
 
