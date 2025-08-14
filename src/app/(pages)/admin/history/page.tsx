57e611c0a9f55752f3483d11484f671c9e182d69
yellowisk <oheitoralmeida2013@gmail.com>
2025-08-14 02:23:22 -0300

    feat(admin/history): convert price to BRL when saving the csv

--- src/app/(pages)/admin/history/page.tsx
+++ src/app/(pages)/admin/history/page.tsx
@@ -1,9 +1,9 @@
-'use client'
-import { useEffect, useState } from 'react';
-import { db } from "@/firebase/firebase-config"
-import { collection, getDocs } from "firebase/firestore"
-import AuxHeader from "@/components/header/auxHeader"
-import { Download, Search } from "lucide-react"
+"use client";
+import { useEffect, useState } from "react";
+import { db } from "@/firebase/firebase-config";
+import { collection, getDocs } from "firebase/firestore";
+import AuxHeader from "@/components/header/auxHeader";
+import { Download, Search } from "lucide-react";
 
 interface pedido {
   id: string;
@@ -23,41 +23,44 @@ interface Item {
 
 function getMonthName(monthNumber: number): string {
   const date = new Date(2000, monthNumber - 1); // JS months are 0-based
-  return date.toLocaleString('pt-BR', { month: 'long' });
+  return date.toLocaleString("pt-BR", { month: "long" });
 }
 
 export default function HistoricoPedidos() {
-
   const [Pedidos, setPedidos] = useState<pedido[]>([]);
   const [mesesDict, setMesesDict] = useState<Record<string, Item[]>>({});
 
-  const [search, setSearch] = useState('');
-  
+  const [search, setSearch] = useState("");
+
+  const formatToBRL = (cents: number) => {
+    return `R$ ${(cents / 100).toFixed(2).replace(".", ",")}`;
+  };
+
   useEffect(() => {
     const getPedidos = async () => {
       const data = await getDocs(collection(db, "purchases"));
-      console.log(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
-      setPedidos(data.docs.map((doc) => ({...doc.data(), id: doc.id})) as pedido[]);
-    }
+      console.log(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      setPedidos(
+        data.docs.map((doc) => ({ ...doc.data(), id: doc.id })) as pedido[]
+      );
+    };
     getPedidos();
-  },[])
+  }, []);
 
   useEffect(() => {
     // Agrupa os pedidos por mês e ano
     const meses: Record<string, Item[]> = {};
-    Pedidos
-    .filter(pedido => (
+    Pedidos.filter((pedido) =>
       pedido.comprador.toLowerCase().includes(search.trim().toLowerCase()) // Filtra por cliente
-    ))
-    .forEach(pedido => {
-      const [, mes, ano] = pedido.dataHora.split('/');
+    ).forEach((pedido) => {
+      const [, mes, ano] = pedido.dataHora.split("/");
       const mesAno = `${ano}/${mes}`; // Formato: "YYYY/MM"
       if (!meses[mesAno]) {
         meses[mesAno] = [];
       }
       meses[mesAno].push({
         cliente: pedido.comprador,
-        produto: pedido.nome, 
+        produto: pedido.nome,
         valor: pedido.preco * pedido.quantidade,
       } as Item);
     });
@@ -66,20 +69,23 @@ export default function HistoricoPedidos() {
   }, [Pedidos, search]);
 
   const downloadCSV = () => {
-    const csvContent = "data:text/csv;charset=utf-8," +
-      "dataHora,comprador,email,nome,quantidade,preco individual\n" +
+    const csvContent =
+      "data:text/csv;charset=utf-8," +
+      "data,comprador,email,nome,quantidade,preco-unidade\n" +
       Pedidos.slice()
-      .sort((a, b) => {
-        // Convert dd/mm/yyyy to Date objects for comparison
-        const [da, ma, ya] = a.dataHora.split('/');
-        const [db, mb, yb] = b.dataHora.split('/');
-        const dateA = new Date(+ya, +ma - 1, +da);
-        const dateB = new Date(+yb, +mb - 1, +db);
-        return dateB.getTime() - dateA.getTime(); // Newest first
-      })
-      .map(pedido =>
-        `"${pedido.dataHora}","${pedido.comprador}","${pedido.email}","${pedido.nome}","${pedido.quantidade}","${pedido.preco}"`
-      ).join("\n");
+        .sort((a, b) => {
+          // Convert dd/mm/yyyy to Date objects for comparison
+          const [da, ma, ya] = a.dataHora.split("/");
+          const [db, mb, yb] = b.dataHora.split("/");
+          const dateA = new Date(+ya, +ma - 1, +da);
+          const dateB = new Date(+yb, +mb - 1, +db);
+          return dateB.getTime() - dateA.getTime(); // Newest first
+        })
+        .map(
+          (pedido) =>
+            `"${pedido.dataHora}","${pedido.comprador}","${pedido.email}","${pedido.nome}","${pedido.quantidade}","${formatToBRL(pedido.preco)}"`
+          )
+        .join("\n");
 
     const encodedUri = encodeURI(csvContent);
     const link = document.createElement("a");
@@ -91,7 +97,6 @@ export default function HistoricoPedidos() {
 
   return (
     <div className="bg-[#FFF4EF] min-h-screen w-full flex flex-col">
-
       <AuxHeader />
 
       <main className="flex flex-col items-center justify-start ">
@@ -100,77 +105,89 @@ export default function HistoricoPedidos() {
             Histórico de Pedidos
           </div>
 
-            <div className="relative flex items-center w-4/5 sm:w-full max-w-xl mx-auto bg-white border border-conpec-blue text-conpec-blue rounded-2xl shadow-md focus-within:text-conpec-primary focus-within:ring-2 focus-within:border-conpec-primary focus-within:ring-[#FF3D00] transition-all">
-              <Search className="absolute left-4 top-1/2 transform -translate-y-1/2" size={22} />
-              <input
-                className="w-full py-3 pl-12 pr-4 text-[16px] rounded-2xl bg-transparent placeholder-conpec-blue text-conpec-primary focus:placeholder-conpec-primary focus:outline-none focus:ring-0"
-                placeholder="Buscar cliente..."
-                value={search}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSearch(e.target.value)}
-              />
-            </div>
-
+          <div className="relative flex items-center w-4/5 sm:w-full max-w-xl mx-auto bg-white border border-conpec-blue text-conpec-blue rounded-2xl shadow-md focus-within:text-conpec-primary focus-within:ring-2 focus-within:border-conpec-primary focus-within:ring-[#FF3D00] transition-all">
+            <Search
+              className="absolute left-4 top-1/2 transform -translate-y-1/2"
+              size={22}
+            />
+            <input
+              className="w-full py-3 pl-12 pr-4 text-[16px] rounded-2xl bg-transparent placeholder-conpec-blue text-conpec-primary focus:placeholder-conpec-primary focus:outline-none focus:ring-0"
+              placeholder="Buscar cliente..."
+              value={search}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setSearch(e.target.value)
+              }
+            />
+          </div>
         </div>
-          
-        
+
         <div className="w-4/5 h-full px-0 md:px-16 py-12  flex-1 flex flex-col justify-start ">
           {mesesDict && Object.keys(mesesDict).length === 0 ? (
             <div className="w-full h-full flex items-center justify-center">
               <div className="text-2xl font-Poppins font-bold text-[#FF9633] text-center">
-              Nenhum pedido encontrado.
+                Nenhum pedido encontrado.
               </div>
             </div>
           ) : (
             <div className="w-full h-full max-w-4xl flex flex-col justify-start">
-              {
-              Object.keys(mesesDict)
+              {Object.keys(mesesDict)
                 .sort((a, b) => b.localeCompare(a))
                 .map((mesAno, index) => {
-                const [ano, mesNum] = mesAno.split("/");
-                const mesNome = getMonthName(parseInt(mesNum) + 1);
-                const items = mesesDict[mesAno]
+                  const [ano, mesNum] = mesAno.split("/");
+                  const mesNome = getMonthName(parseInt(mesNum) + 1);
+                  const items = mesesDict[mesAno];
 
-                const total = items.reduce((sum, item) => sum + ((item.valor)/100), 0);
+                  const total = items.reduce(
+                    (sum, item) => sum + item.valor / 100,
+                    0
+                  );
 
-                return (
-                  <div key={index}>
-                  
-                    <h3 className="text-2xl font-Poppins font-bold text-[#FF9633] uppercase py-6">
-                      {mesNome.toLocaleUpperCase()} – {ano}
-                    </h3>
+                  return (
+                    <div key={index}>
+                      <h3 className="text-2xl font-Poppins font-bold text-[#FF9633] uppercase py-6">
+                        {mesNome.toLocaleUpperCase()} – {ano}
+                      </h3>
 
-                    <div className="pr-6 md:px-10 space-y-2">
-                      {items.map((item, i) => (
-                      <div key={i} className="flex flex-row justify-between font-Poppins font-bold text-xl md:text-2xl space-x-1">
-                        <span className="text-[#FF9633] w-2/6">{item.cliente}</span>
-                        <span className="text-[#FF9633] text-center w-1/2">{item.produto}</span>
-                        <span className="text-[#FF9633] w-1/6">R${(item.valor/100).toFixed(2)}</span>
-                      </div>
-                      ))}
+                      <div className="pr-6 md:px-10 space-y-2">
+                        {items.map((item, i) => (
+                          <div
+                            key={i}
+                            className="flex flex-row justify-between font-Poppins font-bold text-xl md:text-2xl space-x-1"
+                          >
+                            <span className="text-[#FF9633] w-2/6">
+                              {item.cliente}
+                            </span>
+                            <span className="text-[#FF9633] text-center w-1/2">
+                              {item.produto}
+                            </span>
+                            <span className="text-[#FF9633] w-1/6">
+                              R${(item.valor / 100).toFixed(2)}
+                            </span>
+                          </div>
+                        ))}
 
-                      <div className="grid grid-cols-[1fr_1fr_auto] font-Poppins font-extrabold text-[#FF9633] text-2xl ">
-                      <span>Total</span><span></span>
-                      <span>R${total.toFixed(2)}</span>
+                        <div className="grid grid-cols-[1fr_1fr_auto] font-Poppins font-extrabold text-[#FF9633] text-2xl ">
+                          <span>Total</span>
+                          <span></span>
+                          <span>R${total.toFixed(2)}</span>
+                        </div>
                       </div>
                     </div>
-
-                  </div>
-                );
-                })
-              }
+                  );
+                })}
             </div>
           )}
         </div>
 
         <div className="fixed bottom-8 right-8 z-50">
-          <button className="botao-laranja flex flex-col items-center justify-center w-20 h-20 rounded-full shadow-lg"
-            onClick={downloadCSV}>
+          <button
+            className="bg-conpec-primary rounded-full flex flex-col items-center justify-center w-20 h-20 shadow-lg"
+            onClick={downloadCSV}
+          >
             <Download className="text-white" size={60} />
           </button>
         </div>
-
       </main>
-
     </div>
   );
 }
